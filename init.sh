fd09c608f6c4839edbdae6cf811008459a33889b
Gustavo Girão <56512641+Mufado@users.noreply.github.com>
2023-12-04 16:34:01 -0300

    feat(bash): automatic install process

---
+++ init.sh
@@ -0,0 +1,29 @@
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+if ! command_exists git; then
+    echo "ERROR: Missing Git!" >&2
+    exit 1
+fi
+
+if ! command_exists docker; then
+    echo "ERROR: Missing Docker!" >&2
+    exit 1
+fi
+
+if ! command_exists docker-compose; then
+    echo "ERROR: Missing Docker Compose!" >&2
+    exit 1
+fi
+
+if [ ! -e ".executed" ]; then
+    docker compose up -d
+
+    php "migration.php"
+
+    # Cria o marcador para indicar que os comandos foram executados
+    touch .executed
+else
+    echo "Do not execute init.sh again. If you had any problem on installation process, please reclone the repository."
+fi
---
+++ migration.php
@@ -0,0 +1,25 @@
+<?php
+
+$instance = new PDO("mysql:host=mysql;dbname=magazordapi;", "root", "1234");
+
+$sql =
+  "CREATE TABLE `person` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    `cpf` varchar(14) NOT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  
+  CREATE TABLE `contact` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `type` boolean NOT NULL,
+    `description` varchar(255) NOT NULL,
+    `idPerson` int NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `contact_ibfk_2` FOREIGN KEY (`idPerson`) REFERENCES `person` (`id`) ON DELETE CASCADE
+  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
+
+$instance->prepare($sql)->execute();
+
+$sql = null;
+$instance = null;
